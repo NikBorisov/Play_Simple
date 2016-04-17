0aea0accc387a6e38a9102314b9314e52a9cee8e
NikBorisov <nikolboris@gmail.com>
2016-04-17 18:02:41 +0300

    User selection in fileBrowser now higlhlighted

--- app/src/main/java/nikborisov/com/github/play_simple/FileBrowserActivity.java
+++ app/src/main/java/nikborisov/com/github/play_simple/FileBrowserActivity.java
@@ -1,8 +1,11 @@
 package nikborisov.com.github.play_simple;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,7 +14,10 @@ import java.io.File;
 public class FileBrowserActivity extends AppCompatActivity {
 
     private File currentDir;
+    private File selectedDir;
+    private File[] currentDirContent;
     private ListView dirsView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +27,25 @@ public class FileBrowserActivity extends AppCompatActivity {
     }
 
     public void initDirList() {
-        currentDir = Environment.getExternalStorageDirectory();
+        currentDir = Environment.getExternalStorageDirectory(); //start directory is root dir
+        currentDirContent = currentDir.listFiles();//array represents all subdirs in current dir
+
         dirsView = (ListView) findViewById(R.id.currentDirFilesList);
+        dirsView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
         ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
                 ServiceProvider.getMediaFiles(currentDir, false));
         dirsView.setAdapter(listAdapter);
+
+        dirsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                dirsView.getChildAt(position).setBackgroundColor(Color.parseColor("#BBDEFB"));
+            }
+        });
+
+
 
     }
 }
